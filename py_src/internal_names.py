512cead0d23d28972437765844de7e0a66cd5e5c
twoentartian <twoentartian@hotmail.com>
2024-07-08 16:36:48 +0200

    move file

--- py_src/internal_names.py
+++ py_src/internal_names.py
@@ -1,7 +1,7 @@
 
 logger_simulator_base_name = "SimulatorBase"
 py_src_dir_name = "py_src"
-default_config_file_name = "default_config.py"
+default_config_file_name = "config_file_util/default_config.py"
 default_backup_folder_name = "backup"
 log_file_name = "info.log"
 config_module_name = "config"
