302adacf1783d47a78c9f3d4895ebe3084be12cb
twoentartian <twoentartian@hotmail.com>
2024-07-30 14:32:30 +0200

    add a warning of high loss counter

--- result_processing_tools/high_accuracy_trajectory/plot_all_loss.py
+++ result_processing_tools/high_accuracy_trajectory/plot_all_loss.py
@@ -9,12 +9,17 @@ if __name__ == '__main__':
 
     samples_per_df = 200
 
+    high_loss_threshold = 1
+
     # Walk through the current directory and its subdirectories
     for root, dirs, files in os.walk('..'):
         for file in files:
             if file == "loss.csv":
                 file_path = os.path.join(root, file)
                 df = pd.read_csv(file_path)
+                high_loss_count = (df['0'] > high_loss_threshold).sum()
+                if high_loss_count > 0:
+                    print(f"warning: in {file}, counter of loss>{high_loss_threshold} is {high_loss_count}")
                 row = len(df.index)
                 down_sample_rate = (row-1) // samples_per_df + 1
                 df_down_sampled = df.iloc[::down_sample_rate, :]
