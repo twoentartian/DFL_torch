6cca09669fd48ca17f9415f004f1e838db56f7fc
twoentartian <twoentartian@hotmail.com>
2024-08-27 02:45:47 +0200

    add train loss/accuracy

--- tool/measure_model_test_accuracy.py
+++ tool/measure_model_test_accuracy.py
@@ -10,26 +10,42 @@ from py_src import ml_setup
 
 def testing_model(model, current_ml_setup):
     testing_dataset = current_ml_setup.testing_data
+    training_dataset = current_ml_setup.training_data
     criterion = current_ml_setup.criterion
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    dataloader = DataLoader(testing_dataset, batch_size=100, shuffle=True)
+    dataloader_test = DataLoader(testing_dataset, batch_size=100, shuffle=True)
+    dataloader_train = DataLoader(training_dataset, batch_size=100, shuffle=True)
 
     model.eval()
     model.to(device)
     test_loss = 0
+    train_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (data, label) in enumerate(dataloader):
+        for batch_idx, (data, label) in enumerate(dataloader_test):
             data, label = data.to(device), label.to(device)
             outputs = model(data)
             loss = criterion(outputs, label)
-
             test_loss += loss.item() * data.size(0)
             _, predicted = outputs.max(1)
             total += label.size(0)
             correct += predicted.eq(label).sum().item()
-    return test_loss / total, correct / total
+        test_loss = test_loss / total
+        test_accuracy = correct / total
+
+        for batch_idx, (data, label) in enumerate(dataloader_train):
+            data, label = data.to(device), label.to(device)
+            outputs = model(data)
+            loss = criterion(outputs, label)
+            train_loss += loss.item() * data.size(0)
+            _, predicted = outputs.max(1)
+            total += label.size(0)
+            correct += predicted.eq(label).sum().item()
+        train_loss = train_loss / total
+        train_accuracy = correct / total
+
+    return test_loss, test_accuracy, train_loss, train_accuracy
 
 
 if __name__ == "__main__":
@@ -55,5 +71,6 @@ if __name__ == "__main__":
     model = current_ml_setup.model
     model.load_state_dict(torch.load(model_file_path))
 
-    loss, acc = testing_model(model, current_ml_setup)
-    print(f"loss={loss}, acc={acc}")
+    test_loss, test_accuracy, train_loss, train_accuracy = testing_model(model, current_ml_setup)
+    print(f"test loss={test_loss}, test acc={test_accuracy}")
+    print(f"train loss={train_loss}, train acc={train_accuracy}")
