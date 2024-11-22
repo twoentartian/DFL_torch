219410f3e0e625acd84c85c00f3b751a5f377e9f
twoentartian <twoentartian@hotmail.com>
2024-11-22 04:10:14 +0100

    add half label option.

--- py_src/config_file_util/label_distribution.py
+++ py_src/config_file_util/label_distribution.py
@@ -10,3 +10,11 @@ def label_distribution_non_iid_dirichlet(target_node: node.Node, parameters: sim
 
 def label_distribution_default(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters) -> np.ndarray:
     return np.random.dirichlet(np.repeat(0, len(parameters.dataset_label)))
+
+def label_distribution_first_half(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters) -> np.ndarray:
+    size = len(parameters.dataset_label)//2
+    return np.concatenate([np.repeat(1, size), np.repeat(0, len(parameters.dataset_label)-size)], axis=None)
+
+def label_distribution_second_half(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters) -> np.ndarray:
+    size = len(parameters.dataset_label)//2
+    return np.concatenate([np.repeat(0, size), np.repeat(1, len(parameters.dataset_label)-size)], axis=None)
