abdeefa6bb5823f12e7e9b2bb45422d8a5cb322d
twoentartian <twoentartian@hotmail.com>
2025-01-08 15:43:19 +0100

    update default config file.

--- py_src/config_file_util/default_config.py
+++ py_src/config_file_util/default_config.py
@@ -16,8 +16,8 @@ from py_src.model_variance_correct import VarianceCorrector, VarianceCorrectionT
 
 config_name = "default_config"
 
-max_tick = 10000        # total simulation ticks
-save_name = "GL_N50"
+max_tick = 10000  # total simulation ticks
+save_name = "LeNet4__FL_N50"
 force_use_cpu = False
 
 """do you want to put all models in GPU or only keep model stat in memory and share a model in gpu?"""
@@ -25,23 +25,27 @@ force_use_cpu = False
 override_use_model_stat = None
 override_allocate_all_models = None
 
-
 """"""""""" Preset """""""""""
-preset_network = 'GL' # GL, FL, single
+preset_network = 'FL'  # GL, FL, single
 preset_network_size = 50
-preset_network_degree = 8 # only valid for GL
+preset_network_degree = 8  # only valid for GL
 
 """"""""" Global Machine learning related parameters """""""""""
 """ predefined: """
+
+
 def get_ml_setup():
     get_ml_setup.__ml_setup = None
     if get_ml_setup.__ml_setup is None:
         # get_ml_setup.__ml_setup = ml_setup.resnet18_cifar10()
-        get_ml_setup.__ml_setup = ml_setup.lenet5_mnist()
+        get_ml_setup.__ml_setup = ml_setup.lenet4_mnist()
+        # get_ml_setup.__ml_setup = ml_setup.lenet5_mnist()
         # get_ml_setup.__ml_setup = ml_setup.cct7_cifar10()
         # get_ml_setup.__ml_setup = ml_setup.mobilenet_v3_small_cifar10()
         # get_ml_setup.__ml_setup = ml_setup.vgg11_mnist()
+        # get_ml_setup.__ml_setup = ml_setup.simplenet_cifar10()
     return get_ml_setup.__ml_setup
+
 
 """ or self defined: """
 # ml_parameters = ml_setup.MlSetup()
@@ -51,17 +55,22 @@ def get_ml_setup():
 
 
 """"""""""" Dataset related parameters """""""""""
+
+
 def get_optimizer(target_node: node.Node, model: torch.nn.Module, parameters: RuntimeParameters, ml_setup: MlSetup):
     """warning: you are not allowed to change optimizer during simulation when use_model_stat == True"""
     assert model is not None
-    if parameters.phase == SimulationPhase.INITIALIZING:    # init
-        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, weight_decay=0.0005)
+    if parameters.phase == SimulationPhase.INITIALIZING:  # init
+        # optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
         lr_scheduler = None
         return optimizer, lr_scheduler
     return None, None
 
 
 """"""""""" model averaging parameters """""""""""
+
+
 def get_average_algorithm(target_node: node.Node, parameters: RuntimeParameters):
     if preset_network == 'FL':
         return model_average.StandardModelAverager()
@@ -74,7 +83,9 @@ def get_average_algorithm(target_node: node.Node, parameters: RuntimeParameters)
     #     return model_average.StandardModelAverager()
     # return model_average.ConservativeModelAverager(0.9, variance_corrector=variance_correction)
 
-    return model_average.StandardModelAverager()
+    variance_correction = VarianceCorrector(VarianceCorrectionType.FollowOthers)
+    return model_average.ConservativeModelAverager(0.5, variance_corrector=variance_correction)
+
 
 def get_average_buffer_size(target_node: node.Node, parameters: RuntimeParameters):
     neighbors = list(parameters.topology.neighbors(target_node.name))
@@ -84,8 +95,10 @@ def get_average_buffer_size(target_node: node.Node, parameters: RuntimeParameter
 
 """"""""""" Global Topology related parameters """""""""""
 """topology will be updated every tick if not None"""
+
+
 def get_topology(parameters: RuntimeParameters) -> nx.Graph:
-    if parameters.phase == SimulationPhase.INITIALIZING:    # init
+    if parameters.phase == SimulationPhase.INITIALIZING:  # init
         if preset_network == 'FL':
             G: nx.Graph = nx.star_graph(preset_network_size)
             get_topology.current = G
@@ -107,19 +120,38 @@ def get_topology(parameters: RuntimeParameters) -> nx.Graph:
 
     return None
 
+
 """"""""""" Node related parameters """""""""""
 """parameters here will only be initialized once at beginning"""
+
+
 def node_behavior_control(parameters: RuntimeParameters):
     if parameters.phase == SimulationPhase.INITIALIZING:
-        if parameters.current_tick == 0:
-            for node_name, node_target in parameters.node_container.items():
-                node_target.send_model_after_P_training = 1
+        if preset_network == 'FL':
+            if parameters.current_tick == 0:
+                for node_name, node_target in parameters.node_container.items():
+                    node_target.send_model_after_P_training = 1
+                    if node_name == 0:
+                        node_target.enable_training = False
+
+        if preset_network == 'GL':
+            if parameters.current_tick == 0:
+                for node_name, node_target in parameters.node_container.items():
+                    node_target.send_model_after_P_training = 1
+
+        if preset_network == 'single':
+            if parameters.current_tick == 0:
+                for node_name, node_target in parameters.node_container.items():
+                    node_target.send_model_after_P_training = 1
+
 
 """"""""""" Training time related parameters """""""""""
 """this function will be called after training to get the next training tick"""
+
+
 def get_next_training_time(target_node: node.Node, parameters: RuntimeParameters) -> int:
     if preset_network == 'FL':
-        if parameters.phase == SimulationPhase.INITIALIZING:    # init
+        if parameters.phase == SimulationPhase.INITIALIZING:  # init
             if target_node.name == 0:
                 return 0
             else:
@@ -129,12 +161,18 @@ def get_next_training_time(target_node: node.Node, parameters: RuntimeParameters
         if parameters.phase == SimulationPhase.INITIALIZING:  # init
             return 0
         return target_node.next_training_tick + random.randint(8, 12)
+    if preset_network == 'single':
+        if parameters.phase == SimulationPhase.INITIALIZING:  # init
+            return 0
+        return target_node.next_training_tick + 10
 
 
 """"""""""" Dataset related parameters """""""""""
 """label distribution will be updated every tick if not None"""
+
+
 def get_label_distribution(target_node: node.Node, parameters: RuntimeParameters):
-    if parameters.phase == SimulationPhase.INITIALIZING:    # init
+    if parameters.phase == SimulationPhase.INITIALIZING:  # init
         # return label_distribution.label_distribution_non_iid_dirichlet(target_node, parameters, 0.5)
         get_label_distribution.current = label_distribution.label_distribution_iid(target_node, parameters)
         return get_label_distribution.current
@@ -143,12 +181,14 @@ def get_label_distribution(target_node: node.Node, parameters: RuntimeParameters
 
 
 """"""""""" Service related parameters """""""""""
+
+
 def get_service_list():
     service_list = []
 
-    service_list.append(ServiceVarianceRecorder(100, phase=[SimulationPhase.AFTER_AVERAGING]))
-    service_list.append(ServiceTrainingLossRecorder(100))
-    service_list.append(ServiceTestAccuracyLossRecorder(100, 100))
-    service_list.append(ServiceWeightsDifferenceRecorder(100))
+    service_list.append(ServiceVarianceRecorder(20, phase=[SimulationPhase.AFTER_AVERAGING]))
+    service_list.append(ServiceTrainingLossRecorder(20))
+    service_list.append(ServiceTestAccuracyLossRecorder(20, 100))
+    service_list.append(ServiceWeightsDifferenceRecorder(20))
 
     return service_list
