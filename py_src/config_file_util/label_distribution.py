cc4164bc51cdfa23f8350c494dfae7721137473b
twoentartian <twoentartian@hotmail.com>
2025-02-21 01:16:16 +0100

    bug fix

--- py_src/config_file_util/label_distribution.py
+++ py_src/config_file_util/label_distribution.py
@@ -10,9 +10,6 @@ def label_distribution_iid(target_node: node.Node, parameters: simulation_runtim
 def label_distribution_non_iid_dirichlet(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters, alpha) -> np.ndarray:
     return np.random.dirichlet(np.repeat(alpha, len(parameters.dataset_label)))
 
-def label_distribution_default(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters) -> np.ndarray:
-    return np.random.dirichlet(np.repeat(0, len(parameters.dataset_label)))
-
 def label_distribution_first_half(target_node: node.Node, parameters: simulation_runtime_parameters.RuntimeParameters) -> np.ndarray:
     size = len(parameters.dataset_label)//2
     return np.concatenate([np.repeat(1, size), np.repeat(0, len(parameters.dataset_label)-size)], axis=None)
