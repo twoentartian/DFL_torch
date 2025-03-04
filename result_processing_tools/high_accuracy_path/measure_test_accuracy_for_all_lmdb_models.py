26274bb1d8fe228947d39a45e4cd73606d2d0698
twoentartian <twoentartian@hotmail.com>
2025-03-04 21:15:34 +0100

    add measuring lmdb test accuracy

--- result_processing_tools/high_accuracy_path/measure_test_accuracy_for_all_lmdb_models.py
+++ result_processing_tools/high_accuracy_path/measure_test_accuracy_for_all_lmdb_models.py
@@ -3,21 +3,30 @@ import torch
 from torch.utils.data import DataLoader
 import sys
 import os
+import pandas
+import logging
 
 import lmdb
 import argparse
+import concurrent.futures
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from py_src import ml_setup
+from py_src import ml_setup, util
 
-def measure_model_in_lmdb(db_path, arg_ml_setup: ml_setup.MlSetup, arg_test_batch_size, arg_dataloader_worker, output_path):
+logger = logging.getLogger("measure_test_accuracy")
+
+def measure_model_in_lmdb(db_path, arg_ml_setup: ml_setup.MlSetup, arg_test_batch_size, arg_dataloader_worker, output_path, task_name):
+    # logger
+    child_logger = logging.getLogger(f"measure_test_accuracy.{task_name}")
+    util.set_logging(child_logger, task_name)
+
     # Open the LMDB environment
     env = lmdb.open(db_path, readonly=True)
 
     with env.begin() as txn:
         # Get the total number of entries
         total_entries = txn.stat()['entries']
-        print(f"Total number of models: {total_entries}")
+        child_logger.info(f"Total number of models: {total_entries}")
         cursor = txn.cursor()
 
         model = arg_ml_setup.model
@@ -45,20 +54,31 @@ def measure_model_in_lmdb(db_path, arg_ml_setup: ml_setup.MlSetup, arg_test_batc
                 all_ticks.append(int(tick))
             all_ticks = sorted(all_ticks)
 
+            test_accuracy_file_path = os.path.join(output_path, f"full_test_accuracy.csv")
+            test_loss_file_path = os.path.join(output_path, f"full_test_loss.csv")
+            if os.path.exists(test_accuracy_file_path) and os.path.exists(test_loss_file_path):
+                accuracy_df = pandas.read_csv(test_accuracy_file_path)
+                loss_df = pandas.read_csv(test_loss_file_path)
+                accuracy_ticks = accuracy_df["tick"].sort_values().tolist()
+                loss_ticks = loss_df["tick"].sort_values().tolist()
+                if all_ticks == accuracy_ticks and all_ticks == loss_ticks:
+                    child_logger.info(f"Loss and accuracy already generated")
+                    return
+
             # create output files
-            test_accuracy_file = open(os.path.join(output_path, f"full_test_accuracy.csv"), "w+")
+            test_accuracy_file = open(test_accuracy_file_path, "w+")
             header = ",".join(["tick", f"{record_node_name}"])
-            test_accuracy_file.write(header)
-            test_loss_file = open(os.path.join(output_path, f"full_test_loss.csv"), "w+")
+            test_accuracy_file.write(header+ "\n")
+            test_loss_file = open(test_loss_file_path, "w+")
             header = ",".join(["tick", f"{record_node_name}"])
-            test_loss_file.write(header)
+            test_loss_file.write(header+ "\n")
 
             for tick in all_ticks:
                 key = f"{record_node_name}/{tick}.model.pt"
                 key_b = key.encode("utf-8")
                 value = cursor.get(key_b)
 
-                print(f"Model: {key}")
+                child_logger.info(f"Model: {key}")
                 buffer = io.BytesIO(value)
                 state_dict = torch.load(buffer, map_location=device)
                 arg_ml_setup.model.load_state_dict(state_dict)
@@ -94,7 +114,9 @@ def measure_model_in_lmdb(db_path, arg_ml_setup: ml_setup.MlSetup, arg_test_batc
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="measure the model test accuracy in a LMDB file")
-    parser.add_argument("db_path", help="Path to the LMDB database")
+    parser.add_argument("path", help="High accuracy trajectory folder path")
+
+    parser.add_argument("-w", "--worker", type=int, default=1, help='specify how many models to train in parallel')
     parser.add_argument("model_type", type=str)
     parser.add_argument("-d", "--dataset_type", type=str, default=None)
     parser.add_argument("--test_batch_size", type=int, default=100)
@@ -105,9 +127,28 @@ if __name__ == "__main__":
         current_ml_setup = ml_setup.get_ml_setup_from_config(args.model_type)
     else:
         current_ml_setup = ml_setup.get_ml_setup_from_config(args.model_type, dataset_type=args.dataset_type)
-    print(f"Current ML model: {current_ml_setup.model_name}, dataset name: {current_ml_setup.dataset_name}")
+
+    path = os.path.abspath(args.path)
+    worker = args.worker
     test_batch_size = args.test_batch_size
     data_loader_worker = args.dataloader_worker
-    db_path = args.db_path
-    output_path = os.path.dirname(db_path)
-    measure_model_in_lmdb(db_path, current_ml_setup, test_batch_size, data_loader_worker, output_path)
+    folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and "-" in f]
+    print(f"all folders: {folders}")
+
+    output_base_path = os.path.join(path, "temp_test_accuracy")
+    if not os.path.exists(output_base_path):
+        os.mkdir(output_base_path)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=worker) as executor:
+        futures = []
+        for folder in folders:
+            task_name = folder
+            db_path = os.path.join(path, folder, "model_stat.lmdb")
+            output_path = os.path.join(output_base_path, folder)
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+            futures.append(executor.submit(measure_model_in_lmdb, db_path, current_ml_setup, test_batch_size, data_loader_worker, output_path, task_name))
+
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+    executor.shutdown(wait=True)
