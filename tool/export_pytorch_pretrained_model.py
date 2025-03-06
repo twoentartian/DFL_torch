c619d3a08ded9a8133330c9fe75a827e2123d173
twoentartian <twoentartian@hotmail.com>
2025-03-06 21:39:09 +0100

    bug fix

--- tool/export_pytorch_pretrained_model.py
+++ tool/export_pytorch_pretrained_model.py
@@ -3,10 +3,8 @@ import torchvision.models as models
 import os
 import sys
 
-from tool.find_high_accuracy_path_config.find_high_accuracy_path_v2_config_resnet18 import model_name
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from py_src import ml_setup, model_average, model_variance_correct, special_torch_layers, cuda, util, configuration_file
+from py_src import util
 
 if __name__ == "__main__":
     resnet18 = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
