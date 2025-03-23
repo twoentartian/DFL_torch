d563b886d51081ed0b5a39974a9a47ad26fb7293
twoentartian <twoentartian@hotmail.com>
2025-03-23 22:04:13 +0100

    add more log messages.

--- result_processing_tools/high_accuracy_path/visualize_high_accuracy_path_and_generate_PCA_result.py
+++ result_processing_tools/high_accuracy_path/visualize_high_accuracy_path_and_generate_PCA_result.py
@@ -327,7 +327,7 @@ def visualize_all_path(arg_path_folder, arg_output_folder, arg_node_name: int, m
             if layer_name not in layers_and_trajectory.keys():
                 np_size = (len(all_sub_folders) * len(weights_list), weights_list[0].shape[0])
                 logger.info(f"create lmdb layer: {layer_name}, size={np_size}")
-                layers_and_trajectory[layer_name] = np.empty(np_size) # we should allocate all memory now
+                layers_and_trajectory[layer_name] = np.zeros(np_size) # we should allocate all memory now
                 layer_and_trajectory_index[layer_name] = []
                 layers_and_trajectory_length[layer_name] = []
             # layers_and_trajectory[layer_name].extend(weights_list)
