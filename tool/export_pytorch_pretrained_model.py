bce0c57496035e66032c496fc7bf64fe44908ebb
twoentartian <twoentartian@hotmail.com>
2025-05-18 22:50:17 +0200

    bug fix

--- tool/export_pytorch_pretrained_model.py
+++ tool/export_pytorch_pretrained_model.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         os.makedirs(output_path)
 
     resnet18 = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-    util.save_model_state(os.path.join(output_path, "resnet18_imagenet1k.model.pt"), resnet18.state_dict(), model_name=str(ModelType.resnet18_bn))
+    util.save_model_state(os.path.join(output_path, "resnet18_imagenet1k.model.pt"), resnet18.state_dict(), model_name=str(ModelType.resnet18_bn.name))
 
     resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
-    util.save_model_state(os.path.join(output_path, "resnet50_imagenet1k.model.pt"), resnet50.state_dict(), model_name=str(ModelType.resnet50))
+    util.save_model_state(os.path.join(output_path, "resnet50_imagenet1k.model.pt"), resnet50.state_dict(), model_name=str(ModelType.resnet50.name))
