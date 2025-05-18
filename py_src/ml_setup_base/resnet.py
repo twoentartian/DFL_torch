025af1707e3920fa4c510d11aac8d471ebae2fc2
twoentartian <twoentartian@hotmail.com>
2025-05-19 01:20:53 +0200

    add sampler function.

--- py_src/ml_setup_base/resnet.py
+++ py_src/ml_setup_base/resnet.py
@@ -63,10 +63,11 @@ def resnet18_imagenet100(pytorch_preset_version=2, enable_replace_bn_with_group_
     output_ml_setup.get_info_from_dataset(dataset)
     output_ml_setup.training_batch_size = 256
     output_ml_setup.has_normalization_layer = True
-    loss_fn, collate_fn, model_ema_decay, model_ema_steps = get_pytorch_training_imagenet(pytorch_preset_version)
+    loss_fn, collate_fn, model_ema_decay, model_ema_steps, sampler_fn = get_pytorch_training_imagenet(pytorch_preset_version)
     output_ml_setup.criterion = loss_fn
     output_ml_setup.collate_fn = collate_fn
     output_ml_setup.model_ema = (model_ema_decay, model_ema_steps)
+    output_ml_setup.sampler_fn = sampler_fn
     return output_ml_setup
 
 def resnet18_imagenet1k(pytorch_preset_version=2, enable_replace_bn_with_group_norm=False):
@@ -80,15 +81,13 @@ def resnet18_imagenet1k(pytorch_preset_version=2, enable_replace_bn_with_group_n
         output_ml_setup.model = models.resnet18(progress=False, num_classes=1000)
         output_ml_setup.model_name = str(ModelType.resnet18_bn.name)
     output_ml_setup.get_info_from_dataset(dataset)
-
     output_ml_setup.training_batch_size = 256
     output_ml_setup.has_normalization_layer = True
-
-    loss_fn, collate_fn, model_ema_decay, model_ema_steps = get_pytorch_training_imagenet(pytorch_preset_version)
+    loss_fn, collate_fn, model_ema_decay, model_ema_steps, sampler_fn = get_pytorch_training_imagenet(pytorch_preset_version)
     output_ml_setup.criterion = loss_fn
     output_ml_setup.collate_fn = collate_fn
     output_ml_setup.model_ema = (model_ema_decay, model_ema_steps)
-
+    output_ml_setup.sampler_fn = sampler_fn
     return output_ml_setup
 
 
@@ -102,9 +101,9 @@ def resnet50_imagenet1k(pytorch_preset_version=2):
     output_ml_setup.get_info_from_dataset(dataset)
     output_ml_setup.training_batch_size = 256
     output_ml_setup.has_normalization_layer = True
-
-    loss_fn, collate_fn, model_ema_decay, model_ema_steps = get_pytorch_training_imagenet(pytorch_preset_version)
+    loss_fn, collate_fn, model_ema_decay, model_ema_steps, sampler_fn = get_pytorch_training_imagenet(pytorch_preset_version)
     output_ml_setup.criterion = loss_fn
     output_ml_setup.collate_fn = collate_fn
     output_ml_setup.model_ema = (model_ema_decay, model_ema_steps)
+    output_ml_setup.sampler_fn = sampler_fn
     return output_ml_setup
