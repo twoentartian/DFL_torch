c12d39c7ba30d1a0fa7ed0ef5f8796f55d5ccae8
twoentartian <twoentartian@hotmail.com>
2025-05-19 01:33:47 +0200

    bug fix

--- py_src/ml_setup_base/other_setup.py
+++ py_src/ml_setup_base/other_setup.py
@@ -14,18 +14,25 @@ def get_mixup_cutmix(*, mixup_alpha, cutmix_alpha, num_classes):
     return RandomChoice(mixup_cutmix)
 
 """get pytorch training setup, version can be 1 or 2"""
+class collate_fn_inst():
+    def __init__(self, target):
+        self.target = target
+
+    def __call__(self, batch):
+        return self.target(*default_collate(batch))
+
+
 def get_pytorch_training_imagenet(version=2):
     if version == 1:
-        collect_fn = None
+        collate_fn = None
         loss_fn = nn.CrossEntropyLoss()
         model_ema_decay = None
         model_ema_steps = None
         sampler_fn = None
-        return loss_fn, collect_fn, model_ema_decay, model_ema_steps, sampler_fn
+        return loss_fn, collate_fn, model_ema_decay, model_ema_steps, sampler_fn
     elif version == 2:
         mixup_cutmix = get_mixup_cutmix(mixup_alpha=0.2, cutmix_alpha=1.0, num_classes=1000)
-        def collate_fn(batch):
-            return mixup_cutmix(*default_collate(batch))
+        collate_fn = collate_fn_inst(mixup_cutmix)
         loss_fn = nn.CrossEntropyLoss(label_smoothing=0.1)
         model_ema_decay = 0.99998
         model_ema_steps = 32
