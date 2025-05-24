7fa1436e484d0199e5fc34d92c4a2b772c3d2dab
twoentartian <twoentartian@hotmail.com>
2025-05-24 03:21:56 +0200

    add a script to export torch pth model to .model.pt

--- tool/generate_high_accuracy_models_from_script.py
+++ tool/generate_high_accuracy_models_from_script.py
@@ -3,6 +3,7 @@ import os
 import sys
 import random
 import copy
+import hashlib
 import numpy as np
 from datetime import datetime
 from torch.utils.data import DataLoader
@@ -13,17 +14,21 @@ from py_src import ml_setup, complete_ml_setup, util
 from py_src.service import record_model_stat
 
 logger = logging.getLogger("generate_high_accuracy_model_from_script")
+save_model_stat_lmdb = True
+
+def hash_model_state_dict(model):
+    state_dict = model.state_dict()
+    hasher = hashlib.sha256()
+    for key in sorted(state_dict.keys()):
+        tensor = state_dict[key].cpu().numpy().tobytes()
+        hasher.update(key.encode('utf-8'))
+        hasher.update(tensor)
+    return hasher.hexdigest()
 
 if __name__ == "__main__":
     values_wd = [2e-4, 3e-4, 4e-4, 5e-4, 6e-4, 7e-4, 8e-4, 9e-4]
     value_ml_setup = ml_setup.resnet18_cifar100()
     random_seed = 42
-
-    use_amp = True
-
-    # logger
-    util.set_logging(logger, "main")
-    logger.info("logging setup complete")
 
     """random seed"""
     np.random.seed(random_seed)
@@ -34,9 +39,13 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = False
     os.environ["PYTHONHASHSEED"] = str(random_seed)
 
+    use_amp = True
+
+    # logger
+    util.set_logging(logger, "main")
+    logger.info("logging setup complete")
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = copy.deepcopy(value_ml_setup.model)
-    model.to(device)
     dataset = copy.deepcopy(value_ml_setup.training_data)
 
     batch_size = value_ml_setup.training_batch_size
@@ -44,9 +53,26 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=num_worker, persistent_workers=True)
     criterion = value_ml_setup.criterion
 
+    time_now_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S_%f")
+    main_output_folder_path = os.path.join(os.curdir, f"{__file__}_{time_now_str}")
+    os.makedirs(main_output_folder_path, exist_ok=True)
+
     for wd in values_wd:
+        """random seed"""
+        np.random.seed(random_seed)
+        random.seed(random_seed)
+        torch.manual_seed(random_seed)
+        torch.cuda.manual_seed(random_seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        os.environ["PYTHONHASHSEED"] = str(random_seed)
+
+        model = copy.deepcopy(value_ml_setup.model)
+        print(f"hash of init model: {hash_model_state_dict(model)}")
+        model.to(device)
+
         name = f"{value_ml_setup.model_name}_{value_ml_setup.dataset_name}_rs{random_seed}_wd{wd:.1e}"
-        output_folder_path = os.path.join(os.curdir, name)
+        output_folder_path = os.path.join(main_output_folder_path, name)
         if not os.path.exists(output_folder_path):
             os.makedirs(output_folder_path)
         else:
@@ -55,6 +81,7 @@ if __name__ == "__main__":
 
         lr = 0.1
         epochs = 50
+        # epochs = 50
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=wd)
         steps_per_epoch = len(value_ml_setup.training_data) // value_ml_setup.training_batch_size + 1
         lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, lr, steps_per_epoch=steps_per_epoch, epochs=epochs)
@@ -69,6 +96,13 @@ if __name__ == "__main__":
         logger.info(f"begin training")
         if use_amp:
             scaler = torch.cuda.amp.GradScaler()
+
+        if save_model_stat_lmdb:
+            record_model_service = record_model_stat.ModelStatRecorder(1)
+            record_model_service.initialize_without_runtime_parameters([0], output_folder_path, save_format='lmdb', lmdb_db_name=f"0")
+        else:
+            record_model_service = None
+
         for epoch in range(epochs):
             train_loss = 0
             count = 0
@@ -97,6 +131,9 @@ if __name__ == "__main__":
             logger.info(f"epoch[{epoch}] loss={train_loss / count} lrs={lrs}")
             log_file.write(f"{epoch},{train_loss / count},{lrs}" + "\n")
             log_file.flush()
+            if record_model_service is not None:
+                model_stat = model.state_dict()
+                record_model_service.trigger_without_runtime_parameters(epoch, [0], [model_stat])
         logger.info(f"finish training")
         log_file.flush()
         log_file.close()
