247d6c57ff85a83cea9f2a3be316236606461697
twoentartian <twoentartian@hotmail.com>
2025-05-24 03:32:19 +0200

    add a flag to skip testing on training dataset.

--- tool/measure_model_test_accuracy.py
+++ tool/measure_model_test_accuracy.py
@@ -9,14 +9,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from py_src import ml_setup, util
 
 
-def testing_model(model, current_ml_setup):
+def testing_model(model, current_ml_setup, test_training):
     testing_dataset = current_ml_setup.testing_data
     training_dataset = current_ml_setup.training_data
     criterion = current_ml_setup.criterion
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     dataloader_test = DataLoader(testing_dataset, batch_size=100, shuffle=True, num_workers=8, persistent_workers=True)
-    dataloader_train = DataLoader(training_dataset, batch_size=100, shuffle=True, num_workers=8, persistent_workers=True)
+    if test_training:
+        dataloader_train = DataLoader(training_dataset, batch_size=100, shuffle=True, num_workers=8, persistent_workers=True)
+    else:
+        dataloader_train = None
 
     model.eval()
     model.to(device)
@@ -37,18 +40,21 @@ def testing_model(model, current_ml_setup):
         test_loss = test_loss / total
         test_accuracy = correct / total
 
-        for batch_idx, (data, label) in enumerate(dataloader_train):
-            print(f"train batch_idx: {batch_idx}")
-            data, label = data.to(device), label.to(device)
-            outputs = model(data)
-            loss = criterion(outputs, label)
-            train_loss += loss.item() * data.size(0)
-            _, predicted = outputs.max(1)
-            total += label.size(0)
-            correct += predicted.eq(label).sum().item()
-        train_loss = train_loss / total
-        train_accuracy = correct / total
-
+        if dataloader_train is not None:
+            for batch_idx, (data, label) in enumerate(dataloader_train):
+                print(f"train batch_idx: {batch_idx}")
+                data, label = data.to(device), label.to(device)
+                outputs = model(data)
+                loss = criterion(outputs, label)
+                train_loss += loss.item() * data.size(0)
+                _, predicted = outputs.max(1)
+                total += label.size(0)
+                correct += predicted.eq(label).sum().item()
+            train_loss = train_loss / total
+            train_accuracy = correct / total
+        else:
+            train_loss = 0
+            train_accuracy = 0
     return test_loss, test_accuracy, train_loss, train_accuracy
 
 
@@ -57,6 +63,7 @@ if __name__ == "__main__":
     parser.add_argument("model_file", type=str)
     parser.add_argument("-m", "--model_type", type=str, default="auto")
     parser.add_argument("-d", "--dataset_type", type=str, default="default")
+    parser.add_argument("-t", "--training", action="store_true")
 
     args = parser.parse_args()
 
@@ -77,6 +84,6 @@ if __name__ == "__main__":
     model = current_ml_setup.model
     model.load_state_dict(model_stat)
 
-    test_loss, test_accuracy, train_loss, train_accuracy = testing_model(model, current_ml_setup)
+    test_loss, test_accuracy, train_loss, train_accuracy = testing_model(model, current_ml_setup, args.training)
     print(f"test loss={test_loss}, test acc={test_accuracy}")
     print(f"train loss={train_loss}, train acc={train_accuracy}")
