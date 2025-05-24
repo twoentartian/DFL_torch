afd861d1c7dbc9c6880800fc1281fa0e5e04b4a8
twoentartian <twoentartian@hotmail.com>
2025-05-24 03:19:01 +0200

    add a script to export torch pth model to .model.pt

---
+++ tool/torch_vision_pth_to_model_pt.py
@@ -0,0 +1,33 @@
+import argparse
+import torch
+import sys
+import os
+
+from tool.find_high_accuracy_path_config.with_rebuild_norm.find_high_accuracy_path_v2_config_resnet18_cifar100_to_origin import model_name
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from py_src import ml_setup, util
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='torch_vision_pth_to_model_pt', description='Convert torch vision .pth file to .model.pt file')
+    parser.add_argument('pth_path', type=str, help='path to .pth file')
+    parser.add_argument('model_type', type=str, help='model type in .model.pt format')
+    parser.add_argument('--dataset_type', type=str, default='default', help='the dataset type can used to train the model')
+    parser.add_argument("--ema", help='extract the ema model')
+
+
+    args = parser.parse_args()
+
+    cpu = torch.device('cpu')
+    pth_target = torch.load(args.pth_path, map_location=cpu, weights_only=False)
+    if args.ema:
+        model_stat_dict = pth_target['model_ema']
+        output_path = f"{args.pth_path}_ema.model.pt"
+    else:
+        model_stat_dict = pth_target['model']
+        output_path = f"{args.pth_path}.model.pt"
+    current_ml_setup = ml_setup.get_ml_setup_from_config(args.model_type, args.dataset_type)
+    model = current_ml_setup.model
+    model.load_state_dict(model_stat_dict)
+    util.save_model_state(output_path, model_stat_dict, model_name=current_ml_setup.model_name)
+
