b4165394678766d95be8381098ca694de15aeb1e
twoentartian <twoentartian@hotmail.com>
2025-09-23 16:55:48 +0200

    training receip for resnet18 on imagenet is tested not to work.

--- py_src/complete_ml_setup.py
+++ py_src/complete_ml_setup.py
@@ -35,11 +35,12 @@ class FastTrainingSetup(object):
             return optimizer, None, epochs
         elif arg_ml_setup.model_name == str(ModelType.resnet18_bn.name) or arg_ml_setup.model_name == str(ModelType.resnet18_gn.name):
             if "imagenet" in arg_ml_setup.dataset_name:
-                lr = 0.1
-                epochs = 60
-                optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
-                steps_per_epoch = len(arg_ml_setup.training_data) // arg_ml_setup.training_batch_size + 1
-                lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, lr, steps_per_epoch=steps_per_epoch, epochs=epochs)
+                raise NotImplementedError
+                # lr = 0.1
+                # epochs = 60
+                # optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+                # steps_per_epoch = len(arg_ml_setup.training_data) // arg_ml_setup.training_batch_size + 1
+                # lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, lr, steps_per_epoch=steps_per_epoch, epochs=epochs)
             elif str(DatasetType.cifar10.name) == arg_ml_setup.dataset_name:
                 lr = 0.1
                 epochs = 30
