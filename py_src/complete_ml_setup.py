9977905fc7fb5f1d91f6ef499087ac0d62a43ce4
twoentartian <twoentartian@hotmail.com>
2025-12-13 18:58:17 +0100

    add cct_7_3x1_32

--- py_src/complete_ml_setup.py
+++ py_src/complete_ml_setup.py
@@ -334,5 +334,45 @@ class RandomDatasetTrainingSetup(object):
             else:
                 raise not_implemented_error_instance
             return optimizer, lr_scheduler, epochs
+        elif arg_ml_setup.model_name == str(ModelType.cct_7_3x1_32.name):
+            if arg_ml_setup.dataset_name in [DatasetType.cifar10.name, DatasetType.cifar100.name]:
+                # steps_per_epoch = len(arg_ml_setup.training_data) // arg_ml_setup.training_batch_size + 1
+                # weight_decay = 6e-2
+                # initial_lr = 55e-5
+                # warmup_lr = 1e-5
+                # min_lr = 1e-5
+                # warmup_epochs = 10
+                # epochs = 300
+                # cooldown_epochs = 10
+                # warmup_steps = warmup_epochs * steps_per_epoch
+                # cosine_steps = (epochs - warmup_epochs) * steps_per_epoch
+                # cooldown_steps = cooldown_epochs * steps_per_epoch
+                # total_steps = warmup_steps + cosine_steps + cooldown_steps
+                # optimizer = torch.optim.AdamW(model.parameters(), lr=initial_lr, weight_decay=weight_decay)
+                #
+                # def lr_lambda(current_step):
+                #     if current_step < warmup_steps:
+                #         # Linear warmup
+                #         lr = warmup_lr + (initial_lr - warmup_lr) * (current_step / warmup_steps)
+                #     elif current_step < warmup_steps + cosine_steps:
+                #         # Cosine annealing
+                #         t = current_step - warmup_steps
+                #         T = cosine_steps
+                #         lr = min_lr + 0.5 * (initial_lr - min_lr) * (1 + math.cos(math.pi * t / T))
+                #     else:
+                #         # Cooldown phase
+                #         lr = min_lr
+                #     return lr / initial_lr
+                #
+                # lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+                # return optimizer, lr_scheduler, epochs
+                lr = 0.1
+                epochs = 100 if epochs is None else epochs
+                wd = 6e-2 if wd is None else wd
+                optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=wd)
+                lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, lr, steps_per_epoch=steps_per_epoch, epochs=epochs)
+            else:
+                raise not_implemented_error_instance
+            return optimizer, lr_scheduler, epochs
         else:
             raise not_implemented_error_instance
