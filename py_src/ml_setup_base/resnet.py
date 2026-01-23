a19ec18ada1d0de19740cf10d9a45e39c2690470
twoentartian <twoentartian@hotmail.com>
2026-01-23 12:56:08 +0100

    add resnet50_imagenet10k

--- py_src/ml_setup_base/resnet.py
+++ py_src/ml_setup_base/resnet.py
@@ -194,3 +194,20 @@ def resnet50_imagenet1k(pytorch_preset_version=2):
     output_ml_setup.model_ema = (model_ema_decay, model_ema_steps)
     output_ml_setup.sampler_fn = sampler_fn
     return output_ml_setup
+
+def resnet50_imagenet100(pytorch_preset_version=2):
+    output_ml_setup = MlSetup()
+    dataset = ml_setup_dataset.dataset_imagenet1k(pytorch_preset_version)
+
+    output_ml_setup.model = models.resnet50(progress=False, num_classes=100)
+    output_ml_setup.model_name = str(ModelType.resnet50.name)
+    output_ml_setup.model_type = ModelType.resnet50
+    output_ml_setup.get_info_from_dataset(dataset)
+    output_ml_setup.training_batch_size = 256
+    output_ml_setup.has_normalization_layer = True
+    loss_fn, collate_fn, model_ema_decay, model_ema_steps, sampler_fn = get_pytorch_training_imagenet(pytorch_preset_version)
+    output_ml_setup.criterion = loss_fn
+    output_ml_setup.collate_fn = collate_fn
+    output_ml_setup.model_ema = (model_ema_decay, model_ema_steps)
+    output_ml_setup.sampler_fn = sampler_fn
+    return output_ml_setup
