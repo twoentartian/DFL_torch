f10cf8449674c3f00b1e01385d12c0e5bfa294a9
twoentartian <twoentartian@hotmail.com>
2026-01-23 13:02:34 +0100

    add resnet50_imagenet100.

--- py_src/ml_setup.py
+++ py_src/ml_setup.py
@@ -10,7 +10,7 @@ from py_src.ml_setup_base.regnet import regnet_y_400mf_imagenet1k, regnet_x_200m
 from py_src.ml_setup_base.squeezenet import squeezenet1_1_imagenet1k
 from py_src.ml_setup_base.vgg import vgg11_mnist, vgg11_cifar10, vgg11_bn_cifar10, vgg11_bn_imagenet1k
 from py_src.ml_setup_base.resnet import resnet18_cifar10, resnet18_cifar100, resnet18_imagenet100, resnet18_imagenet1k, \
-    resnet50_imagenet1k, resnet34_imagenet1k, resnet18_imagenet1k_sam_mask_random_noise, resnet18_imagenet1k_sam_mask_black, \
+    resnet50_imagenet1k, resnet34_imagenet1k, resnet18_imagenet1k_sam_mask_random_noise, resnet18_imagenet1k_sam_mask_black, resnet50_imagenet100, \
     resnet18_svhn
 from py_src.ml_setup_base.simplenet import simplenet_cifar10, simplenet_cifar100
 from py_src.ml_setup_base.mobilenet import mobilenet_v2_cifar10, mobilenet_v2_cifar100, mobilenet_v3_large_imagenet1k
@@ -33,7 +33,7 @@ __all__ = [ 'MlSetup', 'ModelType', 'DatasetType', 'CriterionType', 'imagenet1k_
             'vgg11_mnist', 'vgg11_cifar10', 'vgg11_bn_cifar10', 'vgg11_bn_imagenet1k',
             'resnet18_cifar10', 'resnet18_cifar100', 'resnet18_imagenet100', 'resnet18_imagenet1k', 'resnet18_svhn',
             'resnet18_imagenet1k_sam_mask_black', 'resnet18_imagenet1k_sam_mask_random_noise',
-            'resnet34_imagenet1k', 'resnet50_imagenet1k',
+            'resnet34_imagenet1k', 'resnet50_imagenet1k', 'resnet50_imagenet100',
             'simplenet_cifar10', 'simplenet_cifar100',
             'mobilenet_v2_cifar10', 'mobilenet_v2_cifar100', 'mobilenet_v3_large_imagenet1k',
             'cct7_3x1_cifar10', 'cct7_3x1_cifar100', 'cct7_7x2_imagenet10', 'cct7_7x2_imagenet100', 'cct7_7x2_imagenet1k', 'cct14_7x2_imagenet1k',
@@ -102,6 +102,9 @@ def get_ml_setup_from_model_type(model_name, dataset_type=DatasetType.default, p
         if dataset_type in [dataset_type.default, dataset_type.imagenet1k]:
             assert pytorch_preset_version is not None
             output_ml_setup = resnet50_imagenet1k(pytorch_preset_version)
+        elif dataset_type in [dataset_type.imagenet100]:
+            assert pytorch_preset_version is not None
+            output_ml_setup = resnet50_imagenet100(pytorch_preset_version)
         else:
             raise NotImplementedError
     elif model_name == ModelType.simplenet:
