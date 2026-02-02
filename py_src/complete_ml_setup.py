04344732f87e6ea2f72975cb33181ede2f72e0c7
twoentartian <twoentartian@hotmail.com>
2026-02-02 01:23:13 +0100

    use cosine Annealing LR for nanoclip+flickr30k (train from scratch).

--- py_src/complete_ml_setup.py
+++ py_src/complete_ml_setup.py
@@ -287,9 +287,11 @@ class FastTrainingSetup(object):
         elif arg_ml_setup.model_name == ModelType.nanoclip_default.name:
             if arg_ml_setup.dataset_name in [DatasetType.flickr30k.name]:
                 epochs = 400
+                weight_decay = 4e-4
+                lr=1e-3
                 optimizer_params = [
-                    {"params": model.img_encoder.parameters(), "lr": model.lr, "weight_decay": model.weight_decay},
-                    {"params": model.txt_encoder.parameters(), "lr": model.lr, "weight_decay": model.weight_decay},
+                    {"params": model.img_encoder.parameters(), "lr": lr, "weight_decay": weight_decay},
+                    {"params": model.txt_encoder.parameters(), "lr": lr, "weight_decay": weight_decay},
                 ]
                 optimizer = torch.optim.AdamW(optimizer_params)
                 # lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
